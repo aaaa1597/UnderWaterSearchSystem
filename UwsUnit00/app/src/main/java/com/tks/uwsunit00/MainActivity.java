3baa1d1d2cad6b0c17ae5541d66290eab90e2447
aaaaa <aaaaa>
2021-11-20 12:43:08 +0900

    GoogleMaps表示実装

--- UwsUnit00/app/src/main/java/com/tks/uwsunit00/MainActivity.java
+++ UwsUnit00/app/src/main/java/com/tks/uwsunit00/MainActivity.java
@@ -1,14 +1,28 @@
 package com.tks.uwsunit00;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 	private final static int REQUEST_PERMISSIONS = 111;
+	private GoogleMap					mMap;
+	private FusedLocationProviderClient	mFlpc;
+	private Marker						mDmyMarker;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +33,47 @@ public class MainActivity extends AppCompatActivity {
 		if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
 			requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_PERMISSIONS);
 		}
+
+		/* SupportMapFragmentを取得し、マップを使用する準備ができたら通知を受取る */
+		SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.frfMap);
+		mapFragment.getMapAsync(this);
+
+		mFlpc = LocationServices.getFusedLocationProviderClient(this);
+		mFlpc.getLastLocation().addOnSuccessListener(this, location -> {
+			if (location != null) {
+				LatLng nowposgps = new LatLng(location.getLatitude(), location.getLongitude());
+				TLog.d("経度:{0} 緯度:{1}", location.getLatitude(), location.getLongitude());
+				TLog.d("拡縮 min:{0} max:{1}", mMap.getMinZoomLevel(), mMap.getMaxZoomLevel());
+
+				/* 初期マーカ削除 */
+				mDmyMarker.remove();
+
+				/* 基点マーカ追加 */
+				mMap.addMarker(new MarkerOptions().position(nowposgps).title("BasePos"));
+
+				/* 基点マーカを中心に */
+				mMap.moveCamera(CameraUpdateFactory.newLatLng(nowposgps));
+				TLog.d("CameraPosition:{0}", mMap.getCameraPosition().toString());
+
+				/* 地図拡大率設定 */
+				TLog.d("拡縮 zoom:{0}",19);
+				mMap.moveCamera(CameraUpdateFactory.zoomTo(19));
+
+				/* 地図俯角 50° */
+				CameraPosition tilt = new CameraPosition.Builder(mMap.getCameraPosition()).tilt(70).build();
+				mMap.moveCamera(CameraUpdateFactory.newCameraPosition(tilt));
+			}
+		});
+	}
+
+	@Override
+	public void onMapReady(@NonNull GoogleMap googleMap) {
+		mMap = googleMap;
+
+		/* 東京スカイツリーの位置でマーカを設定 */
+		LatLng skytree = new LatLng(35.710063, 139.8107);
+		mDmyMarker = mMap.addMarker(new MarkerOptions().position(skytree).title("Marker in SkyTree"));
+		mMap.moveCamera(CameraUpdateFactory.newLatLng(skytree));
 	}
 
 	@Override
