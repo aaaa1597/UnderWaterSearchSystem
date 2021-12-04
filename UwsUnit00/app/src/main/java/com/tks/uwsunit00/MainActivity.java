49a6cc5144d454a64d1cb7962a32dccac709b105
aaaaa <aaaaa>
2021-12-04 17:50:14 +0900

    BLE機能をここから、テーラリングする。

--- UwsUnit00/app/src/main/java/com/tks/uwsunit00/MainActivity.java
+++ UwsUnit00/app/src/main/java/com/tks/uwsunit00/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.bluetooth.le.ScanResult;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -12,7 +13,9 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.RemoteException;
 import android.util.Log;
+import android.widget.Button;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
@@ -27,7 +30,11 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.tks.uws.blecentral.IBleService;
+import com.tks.uws.blecentral.IBleServiceCallback;
 
+import java.text.MessageFormat;
+import java.util.List;
 import java.util.Objects;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -123,7 +130,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 		/* 権限リクエストの結果を取得する. */
 		if (requestCode == REQUEST_PERMISSIONS) {
 			if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-				MsgPopUp.create(MainActivity.this).setErrMsg("失敗しました。\nこのアプリでは、どうしようもないので終了します。").Show(MainActivity.this);
+				MsgPopUp.create(MainActivity.this).setErrMsg("失敗しました。\nこのアプリに、権限を与えて下さい。").Show(MainActivity.this);
 			}
 		}
 		else {
@@ -137,18 +144,153 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
 		Intent intent = new Intent("com.tsk.uws.blecentral.BINDSERVICE");
 		intent.setPackage("com.tks.uws.blecentral");
-		bindService(intent, con, Context.BIND_AUTO_CREATE);
+		bindService(intent, mCon, Context.BIND_AUTO_CREATE);
 	}
 
-	private ServiceConnection con = new ServiceConnection() {
+	/* Serviceコールバック */
+	private IBleService mBleServiceIf;
+	private final ServiceConnection mCon = new ServiceConnection() {
 		@Override
 		public void onServiceConnected(ComponentName name, IBinder service) {
-			Log.d("aaaaa", "onServiceConnected() : 接続できた. name=" + name + " service=" + service);
+			mBleServiceIf = IBleService.Stub.asInterface(service);
+
+			/* コールバック設定 */
+			try { mBleServiceIf.setCallback(mCb); }
+			catch (RemoteException e) { e.printStackTrace(); throw new RuntimeException("AIDL-callback設定で失敗!!"); /* ここで例外が起きたら終了する */}
+
+			TLog.d("aaaaaaaaaaa ひとまず、ここまで動くことを確認したい。");
+
+//			/* BT初期化 */
+//			boolean retinit = initBt();
+//			if(!retinit) return;
+//			TLog.d("BT初期化完了");
+//
+//			/* scan開始 */
+//			boolean retscan = startScan();
+//			if(!retscan) return;
+//			TLog.d("scan開始");
+//
+//			/* 30秒後にscan停止 */
+//			mHandler.postDelayed(() -> {
+//				stopScan();
+//			}, SCAN_PERIOD);
 		}
 
 		@Override
 		public void onServiceDisconnected(ComponentName name) {
-			Log.d("aaaaa", "onServiceConnected() : 切断できた.");
+			TLog.d("onServiceDisconnected() name={0}", name);
+			mBleServiceIf = null;
+		}
+	};
+
+	/* AIDLコールバック */
+	private IBleServiceCallback mCb = new IBleServiceCallback.Stub() {
+		@Override
+		public void notifyScanResultlist() throws RemoteException {
+			List<ScanResult> result = mBleServiceIf.getScanResultlist();
+//			runOnUiThread(() -> mDeviceListAdapter.addDevice(result));
+		}
+
+		@Override
+		public void notifyScanResult() throws RemoteException {
+			ScanResult result = mBleServiceIf.getScanResult();
+//			runOnUiThread(() -> mDeviceListAdapter.addDevice(result));
+//			if(result.getScanRecord() != null && result.getScanRecord().getServiceUuids() != null)
+//				TLog.d("発見!! {0}({1}):Rssi({2}) Uuids({3})", result.getDevice().getAddress(), result.getDevice().getName(), result.getRssi(), result.getScanRecord().getServiceUuids().toString());
+//			else
+//				TLog.d("発見!! {0}({1}):Rssi({2})", result.getDevice().getAddress(), result.getDevice().getName(), result.getRssi());
+		}
+
+		@Override
+		public void notifyScanEnd() throws RemoteException {
+			TLog.d("scan終了");
+			runOnUiThread(() -> {
+//				Button btn = findViewById(R.id.btnScan);
+//				btn.setText("scan開始");
+//				btn.setEnabled(true);
+			});
+		}
+
+		@Override
+		public void notifyGattConnected(String Address) throws RemoteException {
+			/* Gatt接続完了 */
+			TLog.d("Gatt接続OK!! -> Services探検中. Address={0}", Address);
+//			runOnUiThread(() -> { mDeviceListAdapter.setStatus(Address, DeviceListAdapter.ConnectStatus.EXPLORING); });
+		}
+
+		@Override
+		public void notifyGattDisConnected(String Address) throws RemoteException {
+			String logstr = MessageFormat.format("Gatt接続断!! Address={0}", Address);
+//			TLog.d(logstr);
+//			Snackbar.make(findViewById(R.id.root_view), logstr, Snackbar.LENGTH_LONG).show();
+//			runOnUiThread(() -> { mDeviceListAdapter.setStatus(Address, DeviceListAdapter.ConnectStatus.NONE); });
+		}
+
+		@Override
+		public void notifyServicesDiscovered(String Address, int status) throws RemoteException {
+//			if(status == BleService.UWS_NG_GATT_SUCCESS) {
+//				TLog.d("Services発見. -> 対象Serviceかチェック ret={0}", status);
+//				runOnUiThread(() -> { mDeviceListAdapter.setStatus(Address, DeviceListAdapter.ConnectStatus.CHECKAPPLI); });
+//			}
+//			else {
+//				String logstr = MessageFormat.format("Services探索失敗!! 処理終了 ret={0}", status);
+//				TLog.d(logstr);
+//				runOnUiThread(() -> { mDeviceListAdapter.setStatus(Address, DeviceListAdapter.ConnectStatus.NONE); });
+//				Snackbar.make(findViewById(R.id.root_view), logstr, Snackbar.LENGTH_LONG).show();
+//			}
+		}
+
+		@Override
+		public void notifyApplicable(String Address, boolean status) throws RemoteException {
+//			if(status) {
+//				TLog.d("対象Chk-OK. -> 通信準備中 Address={0}", Address);
+//				runOnUiThread(() -> { mDeviceListAdapter.setStatus(Address, DeviceListAdapter.ConnectStatus.TOBEPREPARED); });
+//			}
+//			else {
+//				String logstr = MessageFormat.format("対象外デバイス.　処理終了. Address={0}", Address);
+//				TLog.d(logstr);
+//				runOnUiThread(() -> { mDeviceListAdapter.setStatus(Address, DeviceListAdapter.ConnectStatus.NONE); });
+//				Snackbar.make(findViewById(R.id.root_view), logstr, Snackbar.LENGTH_LONG).show();
+//			}
+		}
+
+		@Override
+		public void notifyReady2DeviceCommunication(String Address, boolean status) throws RemoteException {
+//			if(status) {
+//				String logstr = MessageFormat.format("BLEデバイス通信 準備完了. Address={0}", Address);
+//				TLog.d(logstr);
+//				runOnUiThread(() -> { mDeviceListAdapter.setStatus(Address, DeviceListAdapter.ConnectStatus.READY); });
+//				Snackbar.make(findViewById(R.id.root_view), logstr, Snackbar.LENGTH_LONG).show();
+//			}
+//			else {
+//				String logstr = MessageFormat.format("BLEデバイス通信 準備失敗!! Address={0}", Address);
+//				TLog.d(logstr);
+//				runOnUiThread(() -> { mDeviceListAdapter.setStatus(Address, DeviceListAdapter.ConnectStatus.NONE); });
+//				Snackbar.make(findViewById(R.id.root_view), logstr, Snackbar.LENGTH_LONG).show();
+//			}
+		}
+
+		@Override
+		public void notifyResRead(String Address, int rcvval, int status) throws RemoteException {
+//			String logstr = MessageFormat.format("デバイス読込成功. Address={0} val={1} status={2}", Address, rcvval, status);
+//			TLog.d(logstr);
+//			runOnUiThread(() -> { mDeviceListAdapter.setHertBeat(Address, rcvval); });
+//			Snackbar.make(findViewById(R.id.root_view), logstr, Snackbar.LENGTH_LONG).show();
+		}
+
+		@Override
+		public void notifyFromPeripheral(String Address, int rcvval) throws RemoteException {
+//			String logstr = MessageFormat.format("デバイス通知({0}). Address={1}", rcvval, Address);
+//			TLog.d(logstr);
+//			runOnUiThread(() -> { mDeviceListAdapter.setHertBeat(Address, rcvval); });
+//			Snackbar.make(findViewById(R.id.root_view), logstr, Snackbar.LENGTH_LONG).show();
+		}
+
+		@Override
+		public void notifyError(int errcode, String errmsg) throws RemoteException {
+//			String logstr = MessageFormat.format("ERROR!! errcode={0} : {1}", errcode, errmsg);
+//			TLog.d(logstr);
+//			Snackbar.make(findViewById(R.id.root_view), logstr, Snackbar.LENGTH_LONG).show();
 		}
 	};
 }
