607a7b392c113fb19859f44cf2bdcbb48e41c80e
aaaaa <aaaaa>
2021-12-08 21:17:26 +0900

    権限周り追加

---
+++ UwsMember/app/src/main/java/com/tks/uws/uwsmember/ErrPopUp.java
@@ -0,0 +1,56 @@
+package com.tks.uws.uwsmember;
+
+import android.app.Activity;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+
+import androidx.core.content.res.ResourcesCompat;
+
+public class ErrPopUp extends PopupWindow {
+	/* コンストラクタ */
+	private ErrPopUp(Activity activity) {
+		super(activity);
+	}
+
+	/* windows生成 */
+	public static ErrPopUp create(Activity activity) {
+		ErrPopUp retwindow = new ErrPopUp(activity);
+		View popupView = activity.getLayoutInflater().inflate(R.layout.popup_layout, null);
+		popupView.findViewById(R.id.btnClose).setOnClickListener(v -> {
+//                if (mClosePopup.isShowing())
+//                    mClosePopup.dismiss();
+			android.os.Process.killProcess(android.os.Process.myPid());
+		});
+		retwindow.setContentView(popupView);
+		/* 背景設定 */
+		retwindow.setBackgroundDrawable(ResourcesCompat.getDrawable(activity.getResources(), R.drawable.popup_background, null));
+
+		/* タップ時に他のViewでキャッチされないための設定 */
+		retwindow.setOutsideTouchable(true);
+		retwindow.setFocusable(true);
+
+		/* 表示サイズの設定 今回は幅300dp */
+		float width = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 300, activity.getResources().getDisplayMetrics());
+		retwindow.setWidth((int)width);
+		retwindow.setHeight(WindowManager.LayoutParams.WRAP_CONTENT);
+		return retwindow;
+	}
+
+	/* 文字列設定 */
+	public ErrPopUp setErrMsg(String errmsg) {
+		((TextView)this.getContentView().findViewById(R.id.txtErrMsg)).setText(errmsg);
+		return this;
+	}
+
+	public void Show(Activity activity) {
+		Log.d("aaaaa", "aaaaa Show()");
+		View anchor = ((ViewGroup)activity.findViewById(android.R.id.content)).getChildAt(0);
+		this.showAtLocation(anchor, Gravity.CENTER,0, 0);
+	}
+}
--- UwsMember/app/src/main/java/com/tks/uws/uwsmember/MainActivity.java
+++ UwsMember/app/src/main/java/com/tks/uws/uwsmember/MainActivity.java
@@ -1,14 +1,86 @@
 package com.tks.uws.uwsmember;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+	private final static int		REQUEST_PERMISSIONS = 1111;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
+
+		/* Bluetoothのサポート状況チェック 未サポート端末なら起動しない */
+		if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+			ErrPopUp.create(MainActivity.this).setErrMsg("Bluetoothが、未サポートの端末です。\n終了します。").Show(MainActivity.this);
+		}
+
+		/* 現在値権限とBluetooth権限が許可されていない場合はリクエスト. */
+		if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+				requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.BLUETOOTH_CONNECT}, REQUEST_PERMISSIONS);
+			else
+				requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSIONS);
+		}
+
+		final BluetoothManager bluetoothManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+		BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+		/* Bluetooth未サポート判定 未サポートならエラーpopupで終了 */
+		if (bluetoothAdapter == null)
+			ErrPopUp.create(MainActivity.this).setErrMsg("Bluetoothが、未サポートの端末です。").Show(MainActivity.this);
+		/* Bluetooth ON/OFF判定 -> OFFならONにするようにリクエスト */
+		else if( !bluetoothAdapter.isEnabled()) {
+			Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			ActivityResultLauncher<Intent> startForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+					result -> {
+						if(result.getResultCode() != Activity.RESULT_OK) {
+							ErrPopUp.create(MainActivity.this).setErrMsg("BluetoothがOFFです。ONにして操作してください。\n終了します。").Show(MainActivity.this);
+						}
+						else {
+
+
+
+//							bindBleService();
+						}
+					});
+			startForResult.launch(enableBtIntent);
+		}
+
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+		/* 対象外なので、無視 */
+		if (requestCode != REQUEST_PERMISSIONS) return;
+
+		/* 権限リクエストの結果を取得する. */
+		long ngcnt = Arrays.stream(grantResults).filter(value -> value != PackageManager.PERMISSION_GRANTED).count();
+		if (ngcnt > 0) {
+			ErrPopUp.create(MainActivity.this).setErrMsg("このアプリには必要な権限です。\n再起動後に許可してください。\n終了します。").Show(MainActivity.this);
+			return;
+		}
+		else {
+
+
+
+//			bindBleService();
+		}
 	}
 }
