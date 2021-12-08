17065e4c288f881e6dd9bbdb47aa86f950db559b
aaaaa <aaaaa>
2021-12-08 20:13:05 +0900

    BLEペリフェラル新規作成

---
+++ UwsMember/app/src/main/java/com/tks/uws/uwsmember/ui/main/FragMain.java
@@ -0,0 +1,32 @@
+package com.tks.uws.uwsmember.ui.main;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tks.uws.uwsmember.R;
+
+public class FragMain extends Fragment {
+	private FragMainViewModel mViewModel;
+
+	@Nullable
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.main_fragment, container, false);
+	}
+
+	@Override
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+
+		/***************/
+	}
+}
---
+++ UwsMember/app/src/main/java/com/tks/uws/uwsmember/ui/main/FragMainViewModel.java
@@ -0,0 +1,7 @@
+package com.tks.uws.uwsmember.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class FragMainViewModel extends ViewModel {
+	// TODO: Implement the ViewModel
+}
