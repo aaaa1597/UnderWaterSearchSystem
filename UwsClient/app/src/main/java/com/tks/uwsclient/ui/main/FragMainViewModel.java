865ad0e1ac8fe381ef24ce68d5412f0e6245ea6c
aaaaa <aaaaa>
2021-12-18 17:46:29 +0900

    パッケージ名変更

---
+++ UwsClient/app/src/main/java/com/tks/uwsclient/ui/main/FragMainViewModel.java
@@ -0,0 +1,47 @@
+package com.tks.uwsclient.ui.main;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class FragMainViewModel extends ViewModel {
+	private final MutableLiveData<Double>			mLatitude		= new MutableLiveData<>(0.0);
+	private final MutableLiveData<Double>			mLongitude		= new MutableLiveData<>(0.0);
+	private final MutableLiveData<Integer>			mHearBeat		= new MutableLiveData<>(105);
+	private final MutableLiveData<Integer>			mID				= new MutableLiveData<>(-1);
+	private final MutableLiveData<ConnectStatus>	mStatus			= new MutableLiveData<>(ConnectStatus.NONE);
+	private final MutableLiveData<Boolean>			mPressSetBtn	= new MutableLiveData<>(false);
+	private final MutableLiveData<Boolean>			m1sNotifyFlg	=new MutableLiveData<>(false);
+
+	public MutableLiveData<Double>	Latitude()	{
+		return mLatitude;
+	}
+	public MutableLiveData<Double>	Longitude()	{
+		return mLongitude;
+	}
+	public MutableLiveData<Integer>	HearBeat()	{
+		return mHearBeat;
+	}
+	public void setID(int id)	{
+		mID.setValue(id);
+	}
+	public int getID()	{
+		Integer ret = mID.getValue();
+		return ret == null ? -1 : ret;
+	}
+	public MutableLiveData<ConnectStatus>	ConnectStatus()	{
+		return mStatus;
+	}
+	public MutableLiveData<Boolean> PressSetBtn()	{
+		return mPressSetBtn;
+	}
+	public MutableLiveData<Boolean> Priodic1sNotifyFlg() {
+		return m1sNotifyFlg;
+	}
+	public enum ConnectStatus {
+		NONE,
+		SETTING_ID,		/* ID設定中 */
+		START_ADVERTISE,/* アドバタイズ開始 */
+		ADVERTISING,	/* アドバタイズ中... 接続開始 */
+		CONNECTED,		/* 接続確立 */
+	}
+}
