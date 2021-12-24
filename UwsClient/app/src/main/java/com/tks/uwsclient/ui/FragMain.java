326254a90f09aea2e6f70befa6136a1d1e0bf96b
aaaaa <aaaaa>
2021-12-24 22:26:52 +0900

    RecyclerViewをゆっくり位置移動させるとおかしな位置になってしまう問題を修正。

--- UwsClient/app/src/main/java/com/tks/uwsclient/ui/FragMain.java
+++ UwsClient/app/src/main/java/com/tks/uwsclient/ui/FragMain.java
@@ -139,6 +139,7 @@ public class FragMain extends Fragment {
 			@Override
 			public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
 				int position = parent.getChildAdapterPosition(view);
+				recyclerView.smoothScrollToPosition(position);
 				mViewModel.setSeekerID(position);
 			}
 		});
