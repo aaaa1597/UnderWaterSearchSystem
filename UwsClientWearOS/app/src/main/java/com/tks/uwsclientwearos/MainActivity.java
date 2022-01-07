55a18049570871b5f4f079ae98b07b672d3353c8
aaaaa <aaaaa>
2022-01-07 13:26:39 +0900

    位置情報取得処理実装

--- UwsClientWearOS/app/src/main/java/com/tks/uwsclientwearos/MainActivity.java
+++ UwsClientWearOS/app/src/main/java/com/tks/uwsclientwearos/MainActivity.java
@@ -2,25 +2,74 @@ package com.tks.uwsclientwearos;
 
 import android.Manifest;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.View;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.LinearSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.tks.uwsclientwearos.ui.FragMainViewModel;
 
 import java.util.Arrays;
+import java.util.Locale;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 	private final static int	REQUEST_LOCATION_SETTINGS	= 1111;
 	private final static int	REQUEST_PERMISSIONS	= 2222;
+	private final static int	LOC_UPD_INTERVAL	= 2500;
+	private FusedLocationProviderClient mFusedLocationClient;
+	private final LocationRequest		mLocationRequest = LocationRequest.create().setInterval(LOC_UPD_INTERVAL)
+																					.setFastestInterval(LOC_UPD_INTERVAL/2)
+																					.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+	private final LocationCallback		mLocationCallback = new LocationCallback() {
+		@Override
+		public void onLocationResult(@NonNull LocationResult locationResult) {
+			super.onLocationResult(locationResult);
+			Location location = locationResult.getLastLocation();
+			TLog.d("1秒定期 (緯度:{0} 経度:{1})", String.format(Locale.JAPAN, "%1$.12f", location.getLatitude()), String.format(Locale.JAPAN, "%1$.12f", location.getLongitude()));
+//			mViewModel.Longitude().setValue(location.getLongitude());
+//			mViewModel.Latitude().setValue(location.getLatitude());
+//			mViewModel.HearBeat().setValue((short)(mRandom.nextInt(40)+30));
+		}
+	};
+	private FragMainViewModel			mViewModel;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		/* ViewModelインスタンス取得 */
+		mViewModel = new ViewModelProvider(this).get(FragMainViewModel.class);
+
+		LocationManager lm = (LocationManager)getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+		final boolean gpsEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+		final boolean wifiEnabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+		TLog.d("gpsEnabled={0} wifiEnabled={1}", gpsEnabled, wifiEnabled);
 
 		/* Bluetoothのサポート状況チェック 未サポート端末なら起動しない */
 		if( !getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
@@ -35,6 +84,62 @@ public class MainActivity extends Activity {
 				requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSIONS);
 		}
 
+		TLog.d("aaaaa ここまで");
+		/* 設定の位置情報ON/OFFチェック */
+		LocationSettingsRequest locationSettingsRequest = new LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest).build();
+		SettingsClient settingsClient = LocationServices.getSettingsClient(this);
+		settingsClient.checkLocationSettings(locationSettingsRequest)
+				.addOnSuccessListener(this, locationSettingsResponse -> {
+					TLog.d("aaaaa ここまで来たOK........2");
+//					mViewModel.mIsSettedLocationON = true;
+//					/* Bleサーバへの接続処理開始 */
+//					mViewModel.bindBleService(getApplicationContext(), mCon);
+				})
+				.addOnFailureListener(this, exception -> {
+					int statusCode = ((ApiException)exception).getStatusCode();
+					switch (statusCode) {
+						case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+							try {
+								ResolvableApiException rae = (ResolvableApiException)exception;
+								rae.startResolutionForResult(MainActivity.this, REQUEST_LOCATION_SETTINGS);
+							}
+							catch (IntentSender.SendIntentException sie) {
+								ErrDialog.create(MainActivity.this, "システムエラー!\n再起動で直ることがあります。\n終了します。").show();
+							}
+							break;
+						case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+							ErrDialog.create(MainActivity.this, "このアプリには位置情報をOnにする必要があります。\n再起動後にOnにしてください。\n終了します。").show();
+							break;
+					}
+				});
+
+		final BluetoothManager bluetoothManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+		BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+		/* Bluetooth未サポート判定 未サポートならエラーpopupで終了 */
+		if (bluetoothAdapter == null) {
+			ErrDialog.create(MainActivity.this, "Bluetooth未サポートの端末です。\n終了します。").show();
+		}
+		/* Bluetooth ON/OFF判定 -> OFFならONにするようにリクエスト */
+		else if( !bluetoothAdapter.isEnabled()) {
+			Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			ActivityResultLauncher<Intent> startForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+					result -> {
+						if(result.getResultCode() != Activity.RESULT_OK) {
+							ErrDialog.create(MainActivity.this, "BluetoothがOFFです。ONにして操作してください。\n終了します。").show();
+						}
+						else {
+							TLog.d("aaaaa ここまで来たOK........4");
+//							/* Bleサーバへの接続処理開始 */
+//							mViewModel.bindBleService(getApplicationContext(), mCon);
+						}
+					});
+			startForResult.launch(enableBtIntent);
+		}
+
+		mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+		mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+
+		TLog.d("aaaaa ここまで");
 		/* SeekerIDのlistView定義 */
 		RecyclerView recyclerView = findViewById(R.id.rvw_seekerid);
 		recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
@@ -49,10 +154,16 @@ public class MainActivity extends Activity {
 				if(newState == RecyclerView.SCROLL_STATE_IDLE) {
 					View lview = linearSnapHelper.findSnapView(recyclerView.getLayoutManager());
 					int pos =  recyclerView.getChildAdapterPosition(lview);
+					TLog.d("aaaaa ここまで来たOK........3");
 //					mViewModel.setSeekerID(pos);
 				}
 			}
 		});
+
+		TLog.d("aaaaa ここまで");
+		TLog.d("aaaaa ここまで来たOK........5");
+//		/* Bleサーバへの接続処理開始 */
+//		mViewModel.bindBleService(getApplicationContext(), mCon);
 	}
 
 	@Override
