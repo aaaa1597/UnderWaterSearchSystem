51fba13e7165eb7e3540fb29bca2c9ecf7da2055
aaaaa <aaaaa>
2022-01-07 12:34:12 +0900

    権限要求処理実装完

--- UwsClientWearOS/app/src/main/java/com/tks/uwsclientwearos/MainActivity.java
+++ UwsClientWearOS/app/src/main/java/com/tks/uwsclientwearos/MainActivity.java
@@ -1,8 +1,9 @@
 package com.tks.uwsclientwearos;
 
+import android.Manifest;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import androidx.annotation.NonNull;
@@ -10,15 +11,28 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.LinearSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.Arrays;
+
 public class MainActivity extends Activity {
+	private final static int	REQUEST_LOCATION_SETTINGS	= 1111;
+	private final static int	REQUEST_PERMISSIONS	= 2222;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
 		/* Bluetoothのサポート状況チェック 未サポート端末なら起動しない */
-		if(getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+		if( !getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
 			ErrDialog.create(MainActivity.this.getApplicationContext(), R.string.error_notsupported).show();
+		}
+
+		/* Bluetooth権限が許可されていない場合はリクエスト. */
+		if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+				requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.BLUETOOTH_ADVERTISE, Manifest.permission.BLUETOOTH_CONNECT}, REQUEST_PERMISSIONS);
+			else
+				requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSIONS);
 		}
 
 		/* SeekerIDのlistView定義 */
@@ -40,4 +54,25 @@ public class MainActivity extends Activity {
 			}
 		});
 	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+		/* 対象外なので、無視 */
+		if (requestCode != REQUEST_PERMISSIONS) return;
+
+		/* 権限リクエストの結果を取得する. */
+		long ngcnt = Arrays.stream(grantResults).filter(value -> value != PackageManager.PERMISSION_GRANTED).count();
+		if (ngcnt > 0) {
+			ErrDialog.create(MainActivity.this, "このアプリには必要な権限です。\n再起動後に許可してください。\n終了します。").show();
+			return;
+		}
+		else {
+			TLog.d("aaaaa ここまで来たOK........");
+//			/* Bleサーバへの接続処理開始 */
+//			mViewModel.bindBleService(getApplicationContext(), mCon);
+		}
+	}
+
 }
